ba8472965c2cc896e1f3670ce890210abe1871b9
hyeonseok <mecaman91@gmail.com>
2023-02-06 17:42:18 +0900

    hello uniess

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+        System.out.println("Hello Uniess");
 
         SpringApplication.run(DemoApplication.class, args);
     }
