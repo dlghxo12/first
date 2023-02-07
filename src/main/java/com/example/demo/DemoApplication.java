e1325cae4ab2177ba4dcf605224f095a833b8054
dlghxo12 <124557051+dlghxo12@users.noreply.github.com>
2023-02-07 15:21:41 +0900

    Update DemoApplication.java
    7

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,7 @@ public class DemoApplication {
 
         System.out.println("Hello World");
         System.out.println("Hello 11116");
-        System.out.println("Hello Hello");
+        System.out.println("Hello Hell7");
         System.out.println("Hello Git Test");
         System.out.println("Hello Git Test5");
         System.out.println("Hello Git Test4");
